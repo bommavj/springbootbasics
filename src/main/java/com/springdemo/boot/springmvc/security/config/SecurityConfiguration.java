6d3f4ed7a7d8c500bb8eb4e6a8f95f4034e31baa
Bomma <vbomma@paypalcorp.com>
2017-09-09 14:25:30 -0700

    spring mvc first commit

---
+++ src/main/java/com/springdemo/boot/springmvc/security/config/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+package com.springdemo.boot.springmvc.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll();
+	}
+}
